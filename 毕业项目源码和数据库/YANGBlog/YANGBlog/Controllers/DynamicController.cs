9476cd219af4f233b0aa8676a17ea0b79096eb88
YANGlEi <YANGlEi_97@26.COM>
2020-06-22 22:29:42 +0800

    页面初版本

---
+++ 毕业项目源码和数据库/YANGBlog/YANGBlog/Controllers/DynamicController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace YANGBlog.Controllers
+{
+    public class DynamicController : Controller
+    {
+        // GET: Dynamic
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Dynamics()
+        {
+            return View();
+        }
+        public ActionResult details()
+        {
+            return View();
+        }
+    }
+}
---
+++ 毕业项目源码和数据库/YANGBlog/YANGBlog/Controllers/MessageBoardController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace YANGBlog.Controllers
+{
+    public class MessageBoardController : Controller
+    {
+        // GET: MessageBoard
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
