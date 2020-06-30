fb62b11f96d0da1254ce3dfee51033b494d57116
YANGlEi <YANGlEi_97@26.COM>
2020-06-30 23:45:01 +0800

    评论显示

--- 毕业项目源码和数据库/YANGBlog/YANGBlog/Controllers/DynamicController.cs
+++ 毕业项目源码和数据库/YANGBlog/YANGBlog/Controllers/DynamicController.cs
@@ -3,23 +3,46 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using YANGBlog.Models;
 
 namespace YANGBlog.Controllers
 {
     public class DynamicController : Controller
     {
+        YANGEntities db = new YANGEntities();
         // GET: Dynamic
         public ActionResult Index()
         {
-            return View();
+            int userid = int.Parse(Session["UserID"].ToString());
+            List <Dynamic> dynamic = db.Dynamic.Where(p =>p.UserId== userid&& p.DynamicType == "0").ToList();
+            return View(dynamic);
         }
         public ActionResult Dynamics()
         {
-            return View();
+            int userid = int.Parse(Session["UserID"].ToString());
+            List<Dynamic> dynamic = db.Dynamic.Where(p => p.UserId == userid && p.DynamicType == "1").ToList();
+            return View(dynamic);
         }
-        public ActionResult details()
+        public ActionResult details(int DynamicId)
         {
+            ViewBag.list = db.Dynamic.Find(DynamicId);
+            ViewBag.Clist = db.Comment.Where(p => p.DynamicId == DynamicId);
             return View();
         }
+        [HttpPost]
+        public void Comment(int userToId, int DynamicId, string CommentContent)
+        {
+            Comment comment = new Comment();
+            comment.DynamicId = DynamicId;
+            comment.UserId = userToId;
+            comment.CommentContent = CommentContent;
+            comment.CommentTime = DateTime.Now;
+            db.Comment.Add(comment);
+            db.SaveChanges();
+        }
+        //public ActionResult CommentReply()
+        //{
+
+        //}
     }
 }
--- 毕业项目源码和数据库/YANGBlog/YANGBlog/Controllers/UserController.cs
+++ 毕业项目源码和数据库/YANGBlog/YANGBlog/Controllers/UserController.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using YANGBlog.Models;
 
 namespace YANGBlog.Controllers
 {
     public class UserController : Controller
     {
+        YANGEntities db = new YANGEntities();
         // GET: User
         public ActionResult Index()
         {
@@ -16,6 +18,21 @@ namespace YANGBlog.Controllers
         public ActionResult Login()
         {
             return View();
+        }
+        [HttpPost]
+        public ActionResult Login(string UserName,string PassWord)
+        {
+            //int count = db.UserInfo.Where(p => p.UserName == UserName && p.PassWord == PassWord).ToList().Count();
+            List<UserInfo> list = db.UserInfo.Where(p => p.UserName == UserName && p.PassWord == PassWord).ToList();
+            if (list.Count==1)
+            {
+                Session["UserID"] = list[0].UserId;
+                return RedirectToAction("Index","Dynamic");
+            }
+            else
+            {
+                return Content("<script>alert('密码错误')</script>");
+            }
         }
     }
 }
